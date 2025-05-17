ed1565dde466e41c26baeb430c1cfaf6a75551c6
hugocis <9101917@alumnos.ufv.es>
2025-05-17 18:50:51 +0200

    refactor: Remove unused useState import in FirmasDocente component

--- app/manager/firmas-docente/page.tsx
+++ app/manager/firmas-docente/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
